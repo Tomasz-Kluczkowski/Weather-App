9c4c0123993c7aeb2221c60a742aef60447fbea9
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-05-09 22:42:18 +0100

    backend workout

--- weather_backend.py
+++ weather_backend.py
@@ -2,16 +2,18 @@ import requests
 import string
 import datetime
 
+
 class Report(object):
     """This class will be used to call Open Weather API and gather data for displaying in the GUI.
-    It has no base class and therefore inherits from Python base object.
+        It has no base class and therefore inherits from Python base object.
     
     Args:
-        object -- The class inherits from Python base object.
+        object (object) -- The class inherits from Python base object.
     """
-    #TODO: Introduce database to store locations checked by the user in the past.
-    #TODO: This should only be passed to the database if the response from the API is 200 (valid location was entered)
-    #TODO: Have to add then a combobox/list (?) with selection of previous locations or replace entry box with combobox.
+
+    # TODO: Introduce database to store locations checked by the user in the past.
+    # TODO: This should only be passed to the database if the response from the API is 200 (valid location was entered)
+    # TODO: Have to add then a combobox/list (?) with selection of previous locations or replace entry box with combobox.
 
     def __init__(self):
         """Initialize Report class.
@@ -25,7 +27,6 @@ class Report(object):
         self.w_d_short = {}
         self.w_d_long = {}
 
-
     def get_report(self, location, units):
         """Obtain data in json format from Open Weather and store it in appropriate dictionaries.
         
@@ -36,45 +37,44 @@ class Report(object):
             error
             data
         """
-        self.location = location
-        self.units = units
-        # response = {}
+
         # Key obtained from Open Weather. Required to make any calls to their API.
         api_key = "fa730d41d41ae83226a227a150d927ac"
         # Base URL used to get weather reports. Notice formatting placeholders for report type, location and units.
-        #{0} - space for report type, {1} - space for location, {2} - space for units.
+        # {0} - space for report type, {1} - space for location, {2} - space for units.
         base_url = "http://api.openweathermap.org/data/2.5/{0}?q={1}{2}&APPID="
         # We must remove any gibberish from location string before making a call to the API.
         # For this translate function is the best tool.
+
         punctuation = string.punctuation
-        translator = str.maketrans('', '', punctuation)
-        self.location = self.location.translate(translator)
+        translator = str.maketrans("", "", punctuation)
+        location = location.translate(translator)
+
         # Prefix required to let API know what units are requested by the user.
         units_prefix = "&units="
+
         # List of report types accepted by the API.
         report_types = ["weather", "forecast", "forecast/daily"]
+        # List of dictionaries which will store all the data returned from API call.
+        weather_dicts = [self.w_d_cur, self.w_d_short, self.w_d_long]
 
-        # TODO: ADD COLLECTION OF ALL TYPES OF REPORT AT ONE GO.
-        # REMEMBER TO ERROR CHECK EVERY TIME. USE A FOR LOOP.
-        # STORE DATA AND IN CASE OF A FAILURE WITH FURTHER REPORTS PRESENT WHAT WE MANAGED TO OBTAIN.
+        for report_type, weather_dict in zip(report_types, weather_dicts):
+            try:
+                response = requests.get(base_url.format(report_type, location,
+                                                        units_prefix + units) + api_key)
+            except requests.exceptions.ConnectionError:
+                # TODO: HANDLE ERRORS PROPERLY
+                status = requests.exceptions.ConnectionError
+                return "Unable to establish connection. Please connect to the internet"
+            weather_dict = response.json()
 
-        try:
-            response = requests.get(base_url.format(self.var_report_type.get(), location,
-                                                    units_prefix + self.var_units.get()) + api_key)
-        except requests.exceptions.ConnectionError:
-            self.var_status.set("Unable to establish connection. Please connect to the internet")
-            return
-
-        self.w_d = response.json()
         # had to add int(w_d["cod]) as the output from API is int (for current) or string (for longer forecasts)
-        if int(self.w_d["cod"]) != 200:
-            self.t1.config(state=NORMAL)
-            self.t1.delete(1.0, END)
-            self.t1.config(state=DISABLED)
+        if any(weather_dicts["cod"]) != 200:
+        # if int(self.w_d_cur["cod"]) != 200:
+
             self.var_status.set("Error: {0}, {1}".format(self.w_d["cod"], self.w_d["message"]))
         else:
             self.display_report()
-
 
     def display_report(self):
         """ Displays report converting data from self.w_d dictionary. 
