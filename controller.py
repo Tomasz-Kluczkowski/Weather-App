762328a30c8e0ac5f50298293e478d8740dde447
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-06-20 16:49:43 +0100

    solved utc to local time conversion in whole app

--- controller.py
+++ controller.py
@@ -71,7 +71,7 @@ class Controller(object):
                          "timezone": {}
                          }
 
-        self.debug = 1
+        self.debug = 0
         self.draw_lines = 0
         self.view = None
         self.model = None
