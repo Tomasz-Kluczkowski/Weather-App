f0f07ff1bda13c66de70f484858e9d853fa06efe
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-07-13 18:16:53 +0100

    learning pytest

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from weather_backend import Report
 from controller import Controller
 from weather_gui import WeatherApp
@@ -8,42 +8,11 @@ app.controller = c
 r = Report(c)
 
 
-class TestReport(unittest.TestCase):
-
-    def setUp(self):
-        pass
-    # def test_finish_get_report(self):
-    #     self.fail()
+def test_combo_drop_menu():
+    """Test if list of locations in controller.app_data["api_calls"] is
+            properly generated out of the database record."""
+    r.combo_drop_menu()
+    rows = r.view()
+    view_list = [row[0] for row in rows]
+    self.assertEqual(view_list, c.app_data["api_calls"])
     #
-    # def test_geonames_api(self):ttt
-    #     self.fail()
-    #
-    # def test_open_weather_api(self):
-    #     self.fail()
-    #
-    # def test_finish_get_time(self):
-    #     self.fail()
-    #
-    # def test_finish_get_date(self):
-    #     self.fail()
-    #
-    # def test_finish_deg_conv(self):
-    #     self.fail()
-
-    def test_combo_drop_menu(self):
-        """Test if list of locations in controller.app_data["api_calls"] is
-        properly generated out of the database record."""
-        r.combo_drop_menu()
-        rows = r.view()
-        view_list = [row[0] for row in rows]
-        self.assertEqual(view_list, c.app_data["api_calls"])
-        # self.fail()
-
-    # def test_insert(self):
-    #     self.fail()
-    #
-    # def test_view(self):
-    #     self.fail()
-
-if __name__ == "__main__":
-    unittest.main()
---
+++ tests_unittest/test_report.py
@@ -0,0 +1,49 @@
+import unittest
+from weather_backend import Report
+from controller import Controller
+from weather_gui import WeatherApp
+app = WeatherApp()
+c = Controller()
+app.controller = c
+r = Report(c)
+
+
+class TestReport(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    # def test_finish_get_report(self):
+    #     self.fail()
+    #
+    # def test_geonames_api(self):ttt
+    #     self.fail()
+    #
+    # def test_open_weather_api(self):
+    #     self.fail()
+    #
+    # def test_finish_get_time(self):
+    #     self.fail()
+    #
+    # def test_finish_get_date(self):
+    #     self.fail()
+    #
+    # def test_finish_deg_conv(self):
+    #     self.fail()
+
+    def test_combo_drop_menu(self):
+        """Test if list of locations in controller.app_data["api_calls"] is
+        properly generated out of the database record."""
+        r.combo_drop_menu()
+        rows = r.view()
+        view_list = [row[0] for row in rows]
+        self.assertEqual(view_list, c.app_data["api_calls"])
+        # self.fail()
+
+    # def test_insert(self):
+    #     self.fail()
+    #
+    # def test_view(self):
+    #     self.fail()
+
+if __name__ == "__main__":
+    unittest.main()
