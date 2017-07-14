4de2909fbeb88c453d8c84efe818dece2fab7ada
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-07-14 18:36:47 +0100

    testing in pytest

--- tests/test_report.py
+++ tests/test_report.py
@@ -2,17 +2,22 @@ import pytest
 from weather_backend import Report
 from controller import Controller
 from weather_gui import WeatherApp
+
+
+# Set up classes necessary for testing.
 app = WeatherApp()
-c = Controller()
-app.controller = c
-r = Report(c)
+controller = Controller()
+app.controller = controller
+report = Report(controller)
 
 
 def test_combo_drop_menu():
     """Test if list of locations in controller.app_data["api_calls"] is
             properly generated out of the database record."""
-    r.combo_drop_menu()
-    rows = r.view()
+    report.combo_drop_menu()
+    rows = report.view()
     view_list = [row[0] for row in rows]
-    self.assertEqual(view_list, c.app_data["api_calls"])
-    #
+    assert view_list == controller.app_data["api_calls"]
+
+if __name__ == "__main__":
+    pytest.main()
