8128d6b1d49f796fa267439248460233b3496012
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-07-15 21:38:33 +0100

    Mousewheel use for Linux added.

--- weather_gui.py
+++ weather_gui.py
@@ -497,6 +497,8 @@ class WeatherApp(tk.Tk):
         self.yscrollbar.bind("<Up>", lambda e: self.move_canvas_up())
         self.yscrollbar.bind("<Down>", lambda e: self.move_canvas_down())
         self.main_canvas.bind("<MouseWheel>", self.mouse_wheel)
+        self.main_canvas.bind("<Button-4>", self.mouse_wheel)
+        self.main_canvas.bind("<Button-5>", self.mouse_wheel)
         self.main_canvas.bind("<Up>", lambda e: self.move_canvas_up())
         self.main_canvas.bind("<Down>", lambda e: self.move_canvas_down())
         # Restore keyboard focus to loc_combobox when enter pressed in
