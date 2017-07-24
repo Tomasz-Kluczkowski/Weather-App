a41b11ae8bdaecfee96606e3cb5954a0316c4d24
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2017-07-24 18:27:25 +0100

    initial work on creating Display class.

--- controller.py
+++ controller.py
@@ -75,7 +75,7 @@ class Controller(object):
                          }
         """:type : dict[str, any]"""
 
-        self.debug = 0
+        self.debug = 1
         self.draw_lines = 0
         self.view = None
         self.model = None
