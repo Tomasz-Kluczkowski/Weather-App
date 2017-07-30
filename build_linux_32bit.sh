c3aea42056504b465986c70b204a162d4d8a61f8
tomasz-kluczkowski <tomaszk1@hotmail.co.uk>
2017-07-30 15:57:07 -0700

    modified linux build bash scripts

--- build_linux_32bit.sh
+++ build_linux_32bit.sh
@@ -5,19 +5,25 @@ clear
 
 function success {
 echo Cx_Freeze build completed successfully.
+echo Creating archive for deployment.
+#cd ../..
+mv build_linux_32bit/exe.linux-i686-3.5 build_linux_32bit/Weather_App_Linux_32bit
+cd build_linux_32bit
+tar -cvzf Weather_App_Linux_32bit.tar.gz Weather_App_Linux_32bit
+#scp Weather_App_Linux_64bit.tar.gz kilthar@kilt:C:/Users/Kilthar/Documents/GitHub/Weather-App/build_linux_64bit
 read -rsp $'Press any key to continue...\n' -n 1 key
 }
 
 
 function proceed {
-if [ -d build ] ; then
+if [ -d build_linux_32bit ] ; then
     echo Deleting build directory.
-    rm -rf build
+    rm -rf build_linux_32bit
 fi
 echo Running cx_Freeze script.
-python cx_setup_linux_32bit.py build &
+python cx_setup_linux_32bit.py build -b build_linux_32bit &
 wait
-if [ -d build ] ; then
+if [ -d build_linux_32bit ] ; then
     success
 else
 echo Cx_Freeze build failed. Please check your cx_freeze setup file.
--- build_linux_64bit.sh
+++ build_linux_64bit.sh
@@ -9,22 +9,25 @@ echo Cx_Freeze build completed successfully.
 
 echo Creating archive for deployment.
 #cd ../..
-mv build/exe.linux-x86_64-3.5 build/Weather_App_Linux_64bit
-cd build
+mv build_linux_64bit/exe.linux-x86_64-3.5 build_linux_64bit/Weather_App_Linux_64bit
+cd build_linux_64bit
 tar -cvzf Weather_App_Linux_64bit.tar.gz Weather_App_Linux_64bit
+#scp Weather_App_Linux_64bit.tar.gz kilthar@kilt:C:/Users/Kilthar/Documents/GitHub/Weather-App/build_linux_64bit
 read -rsp $'Press any key to continue...\n' -n 1 key
 }
 
 
 function proceed {
-if [ -d build ] ; then
+if [ -d build_linux_64bit ] ; then
     echo Deleting build directory.
-    rm -rf build
+    rm -rf build_linux_64bit
 fi
+source virtual_envs/python353-64bit/bin/activate
 echo Running cx_Freeze script.
-python cx_setup_linux_64bit.py build &
+python cx_setup_linux_64bit.py build -b build_linux_64bit &
 wait
-if [ -d build ] ; then
+if [ -d build_linux_64bit ] ; then
+    deactivate
     success
 else
 echo Cx_Freeze build failed. Please check your cx_freeze setup file.
